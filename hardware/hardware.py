550a1b0a35b1293fb14b3700ff367f3bb01cf190
pratyush401 <76796354+pratyush401@users.noreply.github.com>
2026-08-08 00:55:07 -0500

    Create hardware.py with initial setup and comments
    Add initial hardware.py file with import and comments

---
+++ hardware/hardware.py
@@ -0,0 +1,4 @@
+from sys.archie import *
+
+# hardwareVar = Hardware("name", "device type (supported types are DEVICE_TYPE.COMPUTE, DEVICE_TYPE.LINK, DEVICE_TYPE.ROUTER, DEVICE_TYPE.STORAGE")
+# hardwareVar.set_entire_configuration({ "config": "value"})
